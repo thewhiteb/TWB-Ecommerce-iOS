f968407e155e7a4c8d33d48ebdd1fd8a17cfcb15
khurramTWB <khurram.ansar@thewhitebtq.com>
2024-08-06 15:59:14 +0400

    Initial Commit

---
+++ TWB-Ecommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TWB-Ecommerce
+//
+//  Created by Khurram Ansar on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TWB-Ecommerce/TWB_EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TWB_EcommerceApp.swift
+//  TWB-Ecommerce
+//
+//  Created by Khurram Ansar on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TWB_EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
