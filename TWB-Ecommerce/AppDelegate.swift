5cd78b50e100400f6a08a550122e49aa9a2e9954
khurramTWB <khurram.ansar@thewhitebtq.com>
2024-09-04 13:16:04 +0400

    Add Facebook Login

---
+++ TWB-Ecommerce/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  TWB-Ecommerce
+//
+//  Created by Khurram Ansar on 04/09/2024.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder {
+
+}
---
+++ TWB-Ecommerce/Features/Login Signup/manager/FBLoginManager.swift
@@ -0,0 +1,8 @@
+//
+//  FBLoginManager.swift
+//  TWB-Ecommerce
+//
+//  Created by Khurram Ansar on 04/09/2024.
+//
+
+import Foundation
