7ce3ab196db4632aa9e9f14386a100fcba099aa1
Hassam <hassam.ali@thewhitebtq.com>
2024-09-18 17:53:44 +0500

    TWB-551 Working on BagView screen

--- TWB-Ecommerce/Features/Bag/BagView.swift
+++ TWB-Ecommerce/Features/Bag/BagView.swift
@@ -17,6 +17,8 @@ struct BagView: View {
             if count == 0 {
                 EmptyBagView()
                 Spacer()
+            } else {
+                BagListingView(items: items)
             }
         }
         .safeAreaInset(edge: .top) {
@@ -61,6 +63,112 @@ struct EmptyBagView: View {
     }
 }
 
+struct BagListingView: View {
+    let items: [BagViewItemModel]
+    var body: some View {
+        ScrollView {
+            ForEach(items) { item in
+                LazyVStack(alignment: .leading, content: {
+                    BagItemView(ietm: item)
+                })
+            }
+        }
+    }
+}
+
+struct BagViewItemModel: Equatable, Hashable, Identifiable {
+    let id: String
+    let price: Float
+    var count: Int
+    let letters: String?
+    let flavour: String?
+    let topper: String
+    let customisedTopper: Bool
+    let vat: Float // Question: is it for a single product or for all product. Like count is 2, then for 2 or for 1
+    let imageURL: String
+    var totalPrice: Float {
+        (Float(count) * price) + vat
+    }
+}
+
+let items: [BagViewItemModel] = [.init(id: "Tube Acrylic 018",
+                                       price: 447,
+                                       count: 1,
+                                       letters: "A",
+                                       flavour: "Vanilla Mistikkah",
+                                       topper: "Congratulations (AED 32",
+                                       customisedTopper: false,
+                                       vat: 10,
+                                       imageURL: ""),
+                                 .init(id: "Long Acrylic Tray 006",
+                                       price: 1053,
+                                       count: 2,
+                                       letters: "A",
+                                       flavour: "Vanilla Mistikkah",
+                                       topper: "Congratulations (AED 32",
+                                       customisedTopper: false,
+                                       vat: 10,
+                                       imageURL: "")]
+
+struct BagItemView: View {
+    @State var ietm: BagViewItemModel
+    var body: some View {
+        HStack {
+            Image(.tube018)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 127, height: 161)
+                .padding(.leading)
+            ZStack {
+                VStack(alignment: .leading) {
+                    Text("Tube Acrylic 018")
+                        .font(.getFont(name: .libreRegular, size: 12))
+                    Text("AED 447")
+                        .font(.getFont(name: .libreRegular, size: 16))
+                    Text("Including VAT")
+                        .font(.getFont(name: .libreRegular, size: 10))
+                    addDeleteView()
+                    horizontalText(firstText: "Letter: ", secondText: "A")
+                    horizontalText(firstText: "Flavour: ", secondText: "Vanilla Mistikkah")
+                    horizontalText(firstText: "Topper: ", secondText: "Congratulations (AED 32)")
+                    Spacer()
+                }
+                .padding(.top, 5)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func horizontalText(firstText: String,
+                        secondText: String) -> some View {
+        HStack {
+            Text(firstText)
+                .font(.getFont(name: .libreRegular, size: 12))
+            Text(secondText)
+                .font(.getFont(name: .libreRegular, size: 12))
+        }
+    }
+
+    @ViewBuilder
+    func addDeleteView() -> some View {
+        HStack(spacing: 18){
+            Button {
+                print("Delete or Minus")
+            } label: {
+                Image(.delete)
+            }
+            Text("1")
+            Button {
+                print("Add")
+            } label: {
+                Image(.add)
+            }
+        }
+        .padding([.leading, .trailing], 5)
+        .border(.black)
+    }
+}
+
 #Preview {
-    BagView(count: 0)
+    BagView(count: 1)
 }
