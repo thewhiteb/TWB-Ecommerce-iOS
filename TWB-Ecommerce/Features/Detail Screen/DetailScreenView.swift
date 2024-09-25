cad1b4b46b3d3830ac4d7286f8bec9a44395582e
khurramTWB <khurram.ansar@thewhitebtq.com>
2024-09-25 13:55:21 +0400

    Initial Commit

---
+++ TWB-Ecommerce/Features/Detail Screen/DetailScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailScreenView.swift
+//  TWB-Ecommerce
+//
+//  Created by Khurram Ansar on 25/09/2024.
+//
+
+import SwiftUI
+
+struct DetailScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailScreenView()
+}
