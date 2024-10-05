5ec68c6baecebb6cb0a7585b8ae8b7b5cb84f17d
khurramTWB <khurram.ansar@thewhitebtq.com>
2024-10-06 03:05:23 +0400

    Plus Button added in AddOns

--- TWB-Ecommerce/Features/Detail Screen/View/Components/SheetComponent/Add To Bag/AddOnItems.swift
+++ TWB-Ecommerce/Features/Detail Screen/View/Components/SheetComponent/Add To Bag/AddOnItems.swift
@@ -14,61 +14,121 @@ struct AddOnItems: View {
     let isCustomizable: Bool
     
     @State private var currentPage = 0
+    @State private var count: Int = 0
+    @State private var isExpanded: Bool = false
+    @State private var isPlusButtonPressed: Bool = false
+    
     
     var body: some View {
-        VStack(spacing: 0) {
-            ZStack {
-                // Image slider using TabView
-                TabView(selection: $currentPage) {
-                    ForEach(0..<images.count, id: \.self) { index in
-                        Image(images[index])
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 170, height: 200) // Fixed height, dynamic width
-                            .clipped()
-                            .tag(index)
-                    }
-                }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never)) // Hide default page indicator
-                .frame(width: 170, height: 200) // Fixed height for image slider
-
-                // Custom page indicator dots
-                VStack {
-                    Spacer()
-                    HStack(spacing: 5) { // Customize the spacing between indicators
+        
+        ZStack (alignment : .bottomTrailing){
+            VStack(spacing: 0) {
+                ZStack {
+                    // Image slider using TabView
+                    TabView(selection: $currentPage) {
                         ForEach(0..<images.count, id: \.self) { index in
-                            Circle()
-                                .frame(width: currentPage == index ? 8 : 6, height: currentPage == index ? 8 : 6)
-                                .foregroundColor(currentPage == index ? .black : .gray)
+                            Image(images[index])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 170, height: 200) // Fixed height, dynamic width
+                                .clipped()
+                                .tag(index)
                         }
                     }
-                    .padding(.bottom, 10)
-                }
-
-                HStack {
-                    Spacer()
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never)) // Hide default page indicator
+                    .frame(width: 170, height: 200) // Fixed height for image slider
                     
+                    // Custom page indicator dots
                     VStack {
-                        TwitterHeart(width: 24, height: 24, imageIcon: "SmallEmptyHeart")
-                            .padding(.top, 15)
-                            .padding(.trailing, 10)
                         Spacer()
+                        HStack(spacing: 5) { // Customize the spacing between indicators
+                            ForEach(0..<images.count, id: \.self) { index in
+                                Circle()
+                                    .frame(width: currentPage == index ? 8 : 6, height: currentPage == index ? 8 : 6)
+                                    .foregroundColor(currentPage == index ? .black : .gray)
+                            }
+                        }
+                        .padding(.bottom, 10)
                     }
+                    
+                    HStack {
+                        Spacer()
+                        
+                        VStack {
+                            TwitterHeart(width: 24, height: 24, imageIcon: "SmallEmptyHeart")
+                                .padding(.top, 15)
+                                .padding(.trailing, 10)
+                            Spacer()
+                        }
+                    }
+                    
                 }
+                .background(Color(hex: "#F5F5F5"))
+                
+                
+                // Item name
+                Text(itemName)
+                    .font(Font.custom("Baskerville", size: 12))
+                    .foregroundColor(.black)
+                    .padding(.top, 10)
+                
+                // Item price
+                Text(itemPrice)
+                    .font(Font.custom("Baskerville", size: 14).weight(.semibold))
+                    .foregroundColor(.black)
+                    .padding(.top, 2)
             }
-            .background(Color(hex: "#F5F5F5"))
+            .frame(width: 170, height: 200)
+            
+            HStack() {
+                if isExpanded {
+                    // Create a tappable area with 30x30 for the minus/trash icon
+                    ZStack {
+                        // Invisible tappable area with .contentShape to capture taps
+                        Color.clear.frame(width: 30, height: 30)
+                            .contentShape(Rectangle())  // Makes the invisible area tappable
+                            .onTapGesture {
+                                withAnimation(.easeInOut) {
+                                    count -= 1
+                                    if count == 0 {
+                                        isExpanded = false
+                                    }
+                                }
+                            }
 
-            // Item name
-            Text(itemName)
-                .font(Font.custom("Baskerville", size: 12))
-                .foregroundColor(.black)
-                .padding(.top, 10)
+                        Image(systemName: count == 1 ? "trash" : "minus")
+                            .foregroundColor(Color.white)
+                            .padding(.leading, 16)
+                    }
 
-            // Item price
-            Text(itemPrice)
-                .font(Font.custom("Baskerville", size: 14).weight(.semibold))
-                .foregroundColor(.black)
-                .padding(.top, 2)
+                    Spacer()
+
+                    Text("\(count)")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+
+                    Spacer()
+                }
+
+                Image(systemName: "plus")
+                    .foregroundColor(Color.white)
+                    .padding(.trailing, isExpanded ? 16 : 0)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            count += 1
+                            if count > 0 {
+                                isExpanded = true
+                            }
+                        }
+                    }
+            }
+            .frame(width: isExpanded ? 150 : 30, height: 30)
+            .background(Color.black)
+            .cornerRadius(isExpanded ? 25 : 50)
+            .padding(.bottom, 10)
+            .padding(.trailing, 10)
+            .padding(.leading, 10)
+            .animation(.easeInOut(duration: 0.3), value: isExpanded)
         }
         .frame(width: 170, height: 200) // Explicitly set frame to avoid expanding
     }
