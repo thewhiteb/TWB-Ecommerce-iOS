556f25ba397ebb6d766235d120de3469351d85eb
Hassam <hassam.ali@thewhitebtq.com>
2024-10-10 16:35:39 +0500

    - trending product structure is updated

--- TWB-Ecommerce/Features/API/TrendingProductAPI.swift
+++ TWB-Ecommerce/Features/API/TrendingProductAPI.swift
@@ -17,36 +17,24 @@ struct TrendingProductAPI: Endpoint {
     var encoding: JSONEncoding = .default
 }
 
-// MARK: - Product
 struct TrendingProduct: Codable {
-    let id: Int
-    let name: String
-    let price: Double
-    let vat: Double
-    let totalPrice: Double
-    let newArrival: Bool
-    let bestSeller: Bool
-    let soldOut: Bool
-    let itemOnSale: Bool
+    let id: Int?
+    let name: String?
+    let price: Double?
+    let vat: Double?
+    let totalPrice: Double?
+    let soldOut: Bool?
+    let itemOnSale: Bool?
     let mainItemSaleDetails: SaleDetails?
-    let popularItem: Bool
-    let discountPercent: Int
-    let fixedPriceDiscount: Int
-    let createdByUser: String
-    let modifiedByUser: String
-    let createdDate: String
-    let mainItemCategoriesLink: String?
-    let mainItemSubItemLink: String?
-    let mainItemImages: [MainItemImage]
-    let mainItemSubCategoriesLink: String?
-
+    let popularItem: Bool?
+    let discountPercent: Int?
+    let fixedPriceDiscount: Int?
+    let mainItemImages: [MainItemImage]?
+    
     enum CodingKeys: String, CodingKey {
-        case id, name, price, vat, totalPrice, newArrival, bestSeller, soldOut, itemOnSale
+        case id, name, price, vat, totalPrice, soldOut, itemOnSale
         case mainItemSaleDetails = "mainItem_SaleDetails"
-        case popularItem, discountPercent, fixedPriceDiscount, createdByUser, modifiedByUser, createdDate
-        case mainItemCategoriesLink = "mainItem_Categories_Link"
-        case mainItemSubItemLink = "mainItem_SubItem_Link"
+        case popularItem, discountPercent, fixedPriceDiscount
         case mainItemImages = "mainItem_Images"
-        case mainItemSubCategoriesLink = "mainItem_SubCategories_Link"
     }
 }
